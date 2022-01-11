79e250ee051bb2ccbd51792b58833a5950517f48
Nayaabkhan Khan <nayaab@hey.com>
2022-01-11 16:40:24 +0530

    Add section to display tweets

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
+const dateFns = require('date-fns')
 
 module.exports = function (config) {
   config.addPassthroughCopy({ 'public/css': 'css' })
@@ -11,6 +12,27 @@ module.exports = function (config) {
   config.addWatchTarget('./src/styles')
 
   config.addPlugin(eleventyNavigationPlugin)
+
+  config.addFilter('tweetImage', (obj, size) => {
+    const result = obj.replace(/_normal.png$/, `_x${size}.png`)
+    return result
+  })
+
+  config.addFilter('tweetUrl', (obj) => {
+    const url = `https://twitter.com/${obj.user.screen_name}/status/${obj.id_str}`
+    return url
+  })
+
+  config.addFilter('tweetRemoveLink', (obj) => {
+    const result = obj.replace(/https:\/\/t.co\/\S*/gm, '')
+    return result
+  })
+
+  config.addFilter('tweetDateFmt', (obj) => {
+    const parsedDate = dateFns.parse(obj, 'E LLL dd HH:mm:ss XXXX yyyy', new Date())
+    const formatted = dateFns.formatDistance(parsedDate, new Date(), { addSuffix: true })
+    return formatted
+  })
 
   return {
     dir: {
---
+++ src/_data/tweets.js
@@ -0,0 +1,35 @@
+const { AssetCache } = require('@11ty/eleventy-cache-assets')
+const Twitter = require('twitter')
+require('dotenv').config()
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  bearer_token: process.env.TWITTER_BEARER_TOKEN,
+})
+
+const params = {
+  screen_name: 'AiryxOS',
+  count: 3,
+  exclude_replies: true,
+  include_rts: false,
+  tweet_mode: 'extended',
+}
+
+module.exports = async function () {
+  const hash = require('crypto').createHash('md5').update(JSON.stringify(params)).digest('hex')
+  let asset = new AssetCache('twitter_timeline_' + hash)
+  if (asset.isCacheValid('1h')) {
+    return asset.getCachedValue()
+  }
+
+  // API Reference:
+  // https://developer.twitter.com/en/docs/twitter-api/v1/tweets/timelines/api-reference/get-statuses-user_timeline
+  const timeline = await client.get('statuses/user_timeline', params).catch((err) => {
+    console.error(err)
+  })
+
+  await asset.save(timeline, 'json')
+
+  return timeline
+}
