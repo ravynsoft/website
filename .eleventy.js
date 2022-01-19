4e28cf7d3450b9017ff20af452f8afc0d8f00e9d
Nayaabkhan Khan <nayaab@hey.com>
2022-01-19 12:47:25 +0530

    Load tweets on client-side
    - Remove build time fetching of tweets
    - Set up a serverless function to fetch tweets instead
    - Use preact and htm to render tweets in the browser
    - Use native date formatting instead of date-fns on client

--- .eleventy.js
+++ .eleventy.js
@@ -14,27 +14,6 @@ module.exports = function (config) {
 
   config.addPlugin(eleventyNavigationPlugin)
 
-  config.addFilter('tweetImage', (obj, size) => {
-    const result = obj.replace(/_normal.png$/, `_x${size}.png`)
-    return result
-  })
-
-  config.addFilter('tweetUrl', (obj) => {
-    const url = `https://twitter.com/${obj.user.screen_name}/status/${obj.id_str}`
-    return url
-  })
-
-  config.addFilter('tweetRemoveLink', (obj) => {
-    const result = obj.replace(/https:\/\/t.co\/\S*/gm, '')
-    return result
-  })
-
-  config.addFilter('tweetDateFmt', (obj) => {
-    const parsedDate = dateFns.parse(obj, 'E LLL dd HH:mm:ss XXXX yyyy', new Date())
-    const formatted = dateFns.formatDistance(parsedDate, new Date(), { addSuffix: true })
-    return formatted
-  })
-
   config.addFilter('githubDateFmt', (obj) => {
     const parsedDate = dateFns.parseISO(obj)
     const formatted = dateFns.format(parsedDate, 'do LLL yyyy')
