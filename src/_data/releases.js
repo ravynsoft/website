a87289e5d6a46b71eaf59e5875e90ce67a093f51
Zoe Knox <zoe@pixin.net>
2022-07-27 18:47:33 -0400

    Replace logo

--- src/_data/releases.js
+++ src/_data/releases.js
@@ -1,7 +1,7 @@
 const Cache = require('@11ty/eleventy-cache-assets')
 
 module.exports = async function () {
-  return Cache('https://api.github.com/repos/mszoek/airyx/releases', {
+  return Cache('https://api.github.com/repos/ravynsoft/ravynos/releases', {
     duration: '1h',
     type: 'json',
   })
