17e5bf9e2d6e4841e1bf17353000c2f33cbff923
Petar Dimic <petargithub@dimic.tech>
2025-12-24 01:37:26 +0100

    worked on seo, centers, updates, etc

---
+++ src/tasks/scripts.11ty.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const path = require('path')
+const esbuild = require('esbuild')
+
+const ENTRY_FILE = 'index.js'
+
+class Scripts {
+    data() {
+        return {
+            permalink: `js/main.js`,
+            eleventyExcludeFromCollections: true,
+        }
+    }
+
+    async render() {
+        try {
+            const entryPoint = path.join(__dirname, `../scripts/${ENTRY_FILE}`)
+
+            const result = await esbuild.build({
+                entryPoints: [entryPoint],
+                bundle: true,
+                minify: true,
+                write: false,
+                target: 'es2020',
+            })
+
+            return result.outputFiles[0].text
+        } catch (err) {
+            console.error(err)
+            return undefined
+        }
+    }
+}
+
+module.exports = Scripts
